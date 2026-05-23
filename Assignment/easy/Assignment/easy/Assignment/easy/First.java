692a2fe99d043fb877c789f48e952c8625926046
Janci11-gif <janci8902@gmail.com>
2026-05-23 21:54:12 +0545

    Create First.java

---
+++ Assignment/easy/Assignment/easy/Assignment/easy/First.java
@@ -0,0 +1,10 @@
+public class First {
+    public static void main(String[] args) {
+        int age= 21;
+        double height= 5.9;
+        String name= "Janci Chaudhary";
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Name: " + name);
+    }
+}
